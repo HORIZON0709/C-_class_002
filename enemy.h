b01a57e0e5c3d8c1303cbb4683c64de654aa60a9
KishimotoEiji <guren0709oresama@gmail.com>
2022-04-27 12:49:05 +0900

    敵の種類を選んで入力

--- enemy.h
+++ enemy.h
@@ -8,13 +8,43 @@
 #define _ENEMY_H_
 
 //********************************
-//敵クラスの定義
+//敵のクラス定義
 //********************************
 class CEnemy
-{
+{/* 基本クラス */
+public:		/* 列挙型の定義 */
+	enum class ENEMY_TYPE
+	{
+		HUMAN = 0,	//人型
+		BIRD,		//鳥型
+		MAX,
+		NONE
+	};
+
 public:		/* コンストラクタ・デストラクタ */
 	CEnemy();
 	~CEnemy();
+
+public:		/* メンバー関数宣言 */
+	void Init();					//初期化
+	void Uninit();					//終了
+	void Input();					//入力
+	void Output();					//出力
+	ENEMY_TYPE SelectEnemyType();	//敵の種類を選ぶ
+
+private:	/* メンバー変数宣言 */
+	int m_nLife;	//体力
+	int m_nAttack;	//攻撃力
+};
+
+//********************************
+//人型の敵のクラス定義
+//********************************
+class CEnemyHuman : public CEnemy
+{/* CEnemyの派生クラス */
+public:		/* コンストラクタ・デストラクタ */
+	CEnemyHuman();
+	~CEnemyHuman();
 
 public:		/* メンバー関数宣言 */
 	void Init();	//初期化
@@ -23,8 +53,25 @@ public:		/* メンバー関数宣言 */
 	void Output();	//出力
 
 private:	/* メンバー変数宣言 */
-	int m_nLife;	//体力
-	int m_nAttack;	//攻撃力
-	int m_nSpeed;	//すばやさ
+	int m_nSpeedWalk;	//歩くスピード
+};
+
+//********************************
+//鳥型の敵のクラス定義
+//********************************
+class CEnemyBird : public CEnemy
+{/* CEnemyの派生クラス */
+public:		/* コンストラクタ・デストラクタ */
+	CEnemyBird();
+	~CEnemyBird();
+
+public:		/* メンバー関数宣言 */
+	void Init();	//初期化
+	void Uninit();	//終了
+	void Input();	//入力
+	void Output();	//出力
+
+private:	/* メンバー変数宣言 */
+	int m_nSpeedFly;	//飛行スピード
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -5,36 +5,58 @@
 //
 //===================================================
 
-//*************************************
+//********************************
 //インクルード
-//*************************************
+//********************************
 #include "main.h"
 #include "enemy.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 //===================================================
 //メイン関数
 //===================================================
 void main(void)
 {
-	CEnemy enemy;	//敵情報
+	CEnemy enemy;			//敵情報
+	CEnemyHuman enemyHuman;	//人型の敵情報
+	CEnemyBird enemyBird;	//鳥型の敵情報
 
-	//初期化
-	enemy.Init();
+	//生成する敵を選択
+	CEnemy::ENEMY_TYPE nEnemyType = enemy.SelectEnemyType();
 
-	//入力
-	enemy.Input();
+	switch (nEnemyType)
+	{
+	case CEnemy::ENEMY_TYPE::HUMAN:		//人型
+		
+		enemyHuman.Init();	//初期化
+		enemyHuman.Input();	//入力
 
-	//画面をクリア
-	system("cls");
+		//画面をクリア
+		system("cls");
 
-	//出力
-	enemy.Output();
+		enemyHuman.Output();	//出力
+		enemyHuman.Uninit();	//終了
+		break;
 
-	//終了
-	enemy.Uninit();
+	case CEnemy::ENEMY_TYPE::BIRD:		//鳥型
+		
+		enemyBird.Init();	//初期化
+		enemyBird.Input();	//入力
+
+		//画面をクリア
+		system("cls");
+
+		enemyBird.Output();	//出力
+		enemyBird.Uninit();	//終了
+		break;
+
+	default:
+		assert(false);
+		break;
+	}
 
 	//終了メッセージ
 	printf("\n プログラムを終了します。お疲れ様でした。");
